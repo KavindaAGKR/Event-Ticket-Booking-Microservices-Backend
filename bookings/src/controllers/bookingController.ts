cf4e96dec5a33d9c9fb3b23ca5d2de60758e3cfd
KavindaAGKR <kavindaaludeniya@gmail.com>
2025-08-27 14:23:56 +0530

    booking controller updated

--- bookings/src/controllers/bookingController.ts
+++ bookings/src/controllers/bookingController.ts
@@ -42,7 +42,7 @@ export const getAllBookings = async (req: Request, res: Response) => {
     const bookings = await getAllBookingsService(userName);
     res.status(200).json({
       status: "SUCCESS",
-      message: "Bookings retrieved successfullyyyy",
+      message: "Bookings retrieved successfully",
       data: bookings,
       
     });
